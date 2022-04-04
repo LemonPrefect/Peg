39e3792c0406f4084a03836330514b766d8c8ed7
LemonPrefect <jingzhuokwok@gmail.com>
2022-04-04 12:29:40 +0800

    [*] Optimized code and comments are added.

--- cli/core/uploader/__init__.py
+++ cli/core/uploader/__init__.py
@@ -23,7 +23,7 @@ class Uploader:
         self.accessKeyId = accessKeyId
         self.secretAccessKey = secretAccessKey
 
-        info = info + '=' * (4 - (len(info) % 4))
+        info += '=' * (4 - (len(info) % 4))
         info = json.loads(base64.b64decode(info))
 
         self.bucket = info.get("bucket", None)
